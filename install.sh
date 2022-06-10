bb2a29839fe50759584e705d373d15a9d8008de3
Mik Mueller <83001409+MikMuellerDev@users.noreply.github.com>
2022-06-10 21:20:57 +0200

    Switch to `Orchis green` theme

--- install.sh
+++ install.sh
@@ -119,7 +119,7 @@ install_arch () {
         $aur -S --needed --noconfirm polybar sway-launcher-desktop bspwm sxhkd dunst \
             alacritty picom nitrogen numlockx slock neovim-remote ly \
             ttf-meslo-nerd-font-powerlevel10k ttf-jetbrains-mono xorg xcursor-breeze \
-            kvantum-theme-layan-git layan-gtk-theme-git kvantum qt5ct ttf-dejavu ttf-liberation \
+            kvantum-theme-orchis-git orchis-theme-git kvantum qt5ct ttf-dejavu ttf-liberation \
             noto-fonts-cjk noto-fonts-emoji noto-fonts-extra tela-icon-theme-green-git \
             network-manager-applet xcolor maim xsct xclip yarn rtkit lxqt-policykit || exit 2
         [ "$is_laptop" = true ] && { $aur -S --needed --noconfirm brightnessctl pamixer || exit 2; }
