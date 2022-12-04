18addd61231ce99876bbde4b6b9fbf4c57dcc549
Mik Mueller <83001409+MikMuellerDev@users.noreply.github.com>
2022-12-04 18:32:26 +0100

    Add clipboard aliases

--- .config/nvim/lua/mikmuellerdev/init.lua
+++ .config/nvim/lua/mikmuellerdev/init.lua
@@ -1,4 +1,8 @@
 _G.is_android = vim.fn.executable('uname') == 1 and vim.fn.system('uname -o') == 'Android\n'
 
+-- Quickclip
+vim.api.nvim_create_user_command('Shareclip', function() io.popen('zsh -i -c "shareclip"') end, {})
+vim.api.nvim_create_user_command('Loadclip', function() io.popen('zsh -i -c "loadclip"') end, {})
+
 require('mikmuellerdev.packer')
 require('mikmuellerdev.mappings')
